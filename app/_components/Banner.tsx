fa3a8b9b69e7502810a172d2e6f4b58c9067f1e5
Daniel Sângelo <daniel.sangelo@assai.com.br>
2026-08-23 16:12:40 +0100

    fix: enhance text opacity and structure in Banner component

--- app/_components/Banner.tsx
+++ app/_components/Banner.tsx
@@ -7,10 +7,17 @@ const Banner = () => {
       <div className="space-y-0.5">
         <div className="space-y-2">
           {/* todo: add dynamic data */}
-          <p>Domingo, 23 de Agosto</p> 
-          <h2 className="text-5xl italic">Mysterious thing, time</h2>
+          <p className="opacity-50">Domingo, 23 de Agosto</p>
+          <div className="border-l-3 px-4 space-y-1">
+            <h2 className="text-5xl italic">Mysterious thing, time.</h2>
+            <p className="italic text-md">
+              Powerful, and when meddled with, dangerous
+            </p>
+          </div>
         </div>
-        <p className="flex justify-end text-sm italic">Albus Dumbledore</p>
+        <p className="flex justify-end text-sm italic opacity-50">
+          Albus Dumbledore
+        </p>
       </div>
       <div className="">
         <Button className="rounded-3xl text-white hover:translate-y-0.5">
