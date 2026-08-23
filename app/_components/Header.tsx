e812ce7425bcdc1171b54d2f25f89f6004d9b79d
Daniel Sângelo <daniel.sangelo@assai.com.br>
2026-08-23 14:32:36 +0100

    feat: add Header component and integrate lucide-react icons

---
+++ app/_components/Header.tsx
@@ -0,0 +1,18 @@
+import { Settings, TimerResetIcon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between w-full">
+      <div className="flex gap-1">
+        <TimerResetIcon size={25} className="text-accent-600" />
+        <h1 className="text-xl font-semibold">
+          task<span className="text-accent-600 font-extrabold">player</span>
+        </h1>
+      </div>
+      <Settings size={19} className="text-accent-600" />
+      {/* todo: dropdown menu for settings */}
+    </div>
+  );
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Header from "./_components/Header";
+
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start border border-accent-400">
+    <div className="flex flex-col flex-1 items-center justify-center bg-white">
+      <main className="flex flex-1 w-full max-w-7xl flex-col items-center justify-between py-16 px-16 bg-white sm:items-start border border-accent-400">
+        <Header />
         <h1 className="text-4xl font-bold text-brand-primary mb-4">
           Tailwind v4 is Live!
         </h1>
