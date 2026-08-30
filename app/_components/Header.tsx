6df924387ba73df4339ccb7f18ad97a9d8af296c
Daniel Sângelo <daniel.sangelo@assai.com.br>
2026-08-30 17:08:29 +0100

    refactor: enhance Header component structure and styling for improved layout

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,17 +1,64 @@
+"use client";
+
 import { Settings, TimerResetIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathName = usePathname();
+
   return (
-    <div className="flex items-center justify-between w-full p-10">
-      <div className="flex gap-1">
-        <TimerResetIcon size={25} className="text-accent-600" />
-        <h1 className="text-xl font-semibold">
-          task<span className="text-accent-600 font-extrabold">player</span>
-        </h1>
-    </div>
-      <Settings size={19} className="text-accent-600" />
-      {/* todo: dropdown menu for settings */}
-    </div>
+    <header className="sticky top-0 z-50 bg-white px-6 py-4 shadow-sm border-b border-secondary-50">
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
+        {/* logo */}
+        <div className="flex p-1">
+          <Link href="/" className="flex gap-1">
+            <TimerResetIcon size={25} className="text-accent-600" />
+            <h1 className="text-xl font-semibold">
+              task<span className="text-accent-600 font-extrabold">player</span>
+            </h1>
+          </Link>
+        </div>
+
+        {/* menus */}
+
+        <nav className="hidden md:flex w-1/3 justify-center">
+          <ul className="flex items-center gap-8">
+            <li>
+              <Link
+                className={`${pathName === "/" ? "relative pb-2 font-semibold text-accent-600 border-b-2 border-accent-600 " : ""}`}
+                href="/"
+              >
+                {" "}
+                Home{" "}
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={`${pathName === "/tasks" ? "relative pb-2 font-semibold text-accent-600 border-b-2 border-accent-600 " : ""}`}
+                href="/tasks"
+              >
+                {" "}
+                Tarefas{" "}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* perfil */}
+        <div>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="cursor-pointer rounded-full"
+          >
+            <Settings size={18} className="text-accent-600" />
+          </Button>
+        </div>
+        {/* todo: dropdown menu for settings */}
+      </div>
+    </header>
   );
 };
 
