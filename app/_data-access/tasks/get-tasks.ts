ef936dcc90e7df249290084c0bceb146e67fc3b9
Daniel Sângelo <daniel.sangelo@assai.com.br>
2026-08-30 14:19:52 +0100

    refactor: modify getTasks function to use raw SQL query for improved task retrieval and ordering

--- app/_data-access/tasks/get-tasks.ts
+++ app/_data-access/tasks/get-tasks.ts
@@ -2,5 +2,24 @@ import { db } from "@/app/_lib/prisma";
 import { Task } from "@/app/generated/prisma/client";
 
 export const getTasks = async (): Promise<Task[]> => {
-  return (await db.task.findMany({ orderBy: { updated_at: 'desc' } }));
+  return await db.$queryRaw`
+    SELECT
+      id,
+      title,
+      description,
+      time_spent::text AS time_spent,
+      started_at,
+      finished_at,
+      created_at,
+      updated_at,
+      user_id,
+      status
+    FROM
+      tasks
+    ORDER BY
+      CASE WHEN status = 'DONE' THEN 99
+        WHEN status = 'RUNNING' THEN 0
+        ELSE 1
+        END,
+      updated_at DESC`
 }
