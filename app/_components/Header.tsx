95cacba3e9a0f9edcccb7f83b20eafc1ad82f3b5
Daniel Sângelo <daniel.sangelo@assai.com.br>
2026-09-06 07:51:15 +0100

    feat: implement responsive header with navigation and dropdown menus

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,12 +1,38 @@
 "use client";
 
-import { Settings, TimerResetIcon } from "lucide-react";
-import { Button } from "./ui/button";
+import { Menu, TimerResetIcon, UserRound } from "lucide-react";
 import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "./ui/dropdown-menu";
+import { Avatar } from "./ui/avatar";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
 import { usePathname } from "next/navigation";
 
 const Header = () => {
-  const pathName = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+  const path = usePathname()
 
   return (
     <header className="sticky top-0 z-50 bg-white px-6 py-4 shadow-sm border-b border-secondary-50">
@@ -22,41 +48,80 @@ const Header = () => {
         </div>
 
         {/* menus */}
-
-        <nav className="hidden md:flex w-1/3 justify-center">
-          <ul className="flex items-center gap-8">
-            <li>
-              <Link
-                className={`${pathName === "/" ? "relative pb-2 font-semibold text-accent-600 border-b-2 border-accent-600 " : ""}`}
-                href="/"
-              >
-                {" "}
-                Home{" "}
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={`${pathName === "/tasks" ? "relative pb-2 font-semibold text-accent-600 border-b-2 border-accent-600 " : ""}`}
-                href="/tasks"
-              >
-                {" "}
-                Tasks{" "}
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <NavigationMenu className="hidden md:flex">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink className={`${navigationMenuTriggerStyle()} ${path === '/' ? 'relative font-semibold border-b-2 border-accent-700 text-primary-700' : ''}`}>
+                <Link href="/">Home</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink className={`${navigationMenuTriggerStyle()} ${path === '/tasks' ? 'relative font-semibold border-b-2 border-accent-700 text-primary-700' : ''}`}>
+                <Link href="/tasks">Tasks</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <div className="md:hidden flex ml-auto">
+          <Sheet open={isOpen || false} onOpenChange={setIsOpen}>
+            <SheetTrigger
+              render={
+                <Button variant="none" size="sm">
+                  <Menu />
+                </Button>
+              }
+            ></SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Menu</SheetTitle>
+              </SheetHeader>
+              <nav className="flex flex-col space-y-4 mt-4 ml-4">
+                <Link
+                  href="/"
+                  className="hover:bg-secondary hover:text-primary-700 p-2"
+                  onClick={() => setIsOpen(false)}
+                >
+                  Home
+                </Link>
+                <Link
+                  href="/tasks"
+                  className="hover:bg-secondary hover:text-primary-700 p-2"
+                  onClick={() => setIsOpen(false)}
+                >
+                  Tasks
+                </Link>
+              </nav>
+            </SheetContent>
+          </Sheet>
+        </div>
 
         {/* perfil */}
-        <div>
-          <Button
-            variant="ghost"
-            size="icon"
-            className="cursor-pointer rounded-full"
-          >
-            <Settings size={18} className="text-accent-600" />
-          </Button>
+        <div className="ml-6">
+          <DropdownMenu>
+            <DropdownMenuTrigger
+              render={
+                <Avatar
+                  className="flex justify-center items-center border-accent-900"
+                  size="default"
+                >
+                  <UserRound className="text-accent-600" />
+                </Avatar>
+              }
+            />
+            <DropdownMenuContent>
+              <DropdownMenuGroup>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuItem className="hover:bg-secondary hover:text-primary-700">Profile</DropdownMenuItem>
+                <DropdownMenuItem className="hover:bg-secondary hover:text-primary-700">Billing</DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem className="hover:bg-secondary hover:text-primary-700">Team</DropdownMenuItem>
+                <DropdownMenuItem className="hover:bg-secondary hover:text-primary-700">Subscription</DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
-        {/* todo: dropdown menu for settings */}
       </div>
     </header>
   );
