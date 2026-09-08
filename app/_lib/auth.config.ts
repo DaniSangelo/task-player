42b9641525d4bdc44ccaf07af22d9c9e756d1812
Daniel Sângelo <daniel.sangelo@assai.com.br>
2026-09-08 20:04:12 +0100

    feat: add JWT callbacks for token and session management in auth configuration

--- app/_lib/auth.config.ts
+++ app/_lib/auth.config.ts
@@ -14,6 +14,16 @@ export const authConfig = {
       },
     })
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.id = user.id;
+      return token;
+    },
+    async session({ session, token }) {
+      if (token && session.user) session.user.id = token.id as string;
+      return session;
+    },
+  },
   session: {
     strategy: 'jwt',
   },
