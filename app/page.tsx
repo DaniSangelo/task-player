b0964e4aea5d92d1bbae47cc6e069b7a357f7456
Daniel Sângelo <daniel.sangelo@assai.com.br>
2026-09-08 12:49:28 +0100

    fix: redirect to login page instead of rendering banner in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { redirect } from "next/navigation";
 import Banner from "./_components/Banner";
 
 export default async function Home() {
-  return <Banner />;
+  redirect('/auth/login');
+  // return <Banner />;
 }
